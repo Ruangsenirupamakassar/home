fd16513a61bf4644f79c3987fd43e7a68d0213fe
Erlin Marbella <erlinmarvella@gmail.com>
2023-05-14 21:47:08 +0800

    Kode website Ruang Seni Rupa Makassar

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: false,
+  pagination: {
+    el: ".swiper-pagination active",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+
